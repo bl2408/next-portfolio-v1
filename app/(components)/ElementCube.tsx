cc42c677baa673ac43adaa0aa1ceac0d6322a4c4
Brian Lambert <brianlambert2408@gmail.com>
2023-04-27 14:50:34 +1000

    adjusted margin bottom

--- app/(components)/ElementCube.tsx
+++ app/(components)/ElementCube.tsx
@@ -2,9 +2,9 @@ export default function ElementCube({ label }: { label: string }) {
 	return (
 		<>
 			<div
-				className={`trans-col-main-theme grid aspect-square
+				className={`mb-4 grid aspect-square
 				w-[30px] scale-75 place-content-center bg-zinc-900 font-mono 
-				font-bold text-neutral-100 dark:bg-neutral-50 dark:text-zinc-800
+				font-bold text-neutral-100 trans-col-main-theme dark:bg-neutral-50 dark:text-zinc-800
 				`}>
 				{label}
 			</div>
