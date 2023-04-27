cac00eff31440c4226b4bf18fe396b21f176acb5
Brian Lambert <brianlambert2408@gmail.com>
2023-04-27 14:33:15 +1000

    Tooltips added

--- app/(components)/Aside.tsx
+++ app/(components)/Aside.tsx
@@ -11,26 +11,38 @@ export default function Aside() {
 					{
 						icon: faAt,
 						href: "/",
+						label: "Email",
 					},
 					{
 						icon: faGithub,
 						href: "/",
+						label: "GitHub",
 					},
 					{
 						icon: faLinkedin,
 						href: "/",
+						label: "LinkedIn",
 					},
 					{
 						icon: faCode,
 						href: "/",
+						label: "Source",
 					},
 				].map((obj, i) => (
-					<li key={i} className="">
-						<Link href={obj.href} className="[&>*]:hover:scale-125">
+					<li key={i} className="relative ">
+						<Link
+							href={obj.href}
+							className=" [&>*]:hover:scale-125 [&>div]:hover:md:left-[300%]">
 							<FontAwesomeIcon
 								icon={obj.icon}
 								className="h-full w-[16px] p-1 transition-transform"
 							/>
+							<div className="absolute flex scale-0 items-center transition-all duration-200 max-md:hidden md:left-[200%] md:top-0">
+								<div className="mr-[-1px] h-0 w-0 [border-bottom:5px_solid_transparent] [border-top:5px_solid_transparent] [border-right:6px_solid_black] dark:border-r-white"></div>
+								<div className="bg-black px-2 py-1 text-[.5rem] font-bold uppercase text-white dark:bg-white dark:text-black">
+									{obj.label}
+								</div>
+							</div>
 						</Link>
 					</li>
 				))}
