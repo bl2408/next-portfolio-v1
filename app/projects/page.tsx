243820e27d6fdfe2c2f9ea5cadddd3b71d69e785
Brian Lambert <brianlambert2408@gmail.com>
2023-05-16 00:03:56 +1000

    Basic static landing page

--- app/projects/page.tsx
+++
@@ -1,6 +0,0 @@
-import SectionProjects from "@/components/SectionProjects";
-import ElementCube from "@/components/misc/ElementCube";
-
-export default async function Page() {
-	return <>TEST</>;
-}
