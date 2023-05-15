80076c802568df37edffa4a024bd373603db8e9f
Brian Lambert <brianlambert2408@gmail.com>
2023-05-16 00:03:56 +1000

    Basic static landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Splash from "@/components/Splash";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+// export const dynamic = "force-dynamic";
+// export const revalidate = 0;
 
 export default function Page() {
 	return (
