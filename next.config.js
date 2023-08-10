4367670ef6a9fb3d599e0dfc50ac8404b40073be
Brian Lambert <brianlambert2408@gmail.com>
2023-08-10 14:39:03 +1000

    updated resume section

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/cv",
+				destination: "https://resume.brianlambert.dev/",
+				basePath: false,
+			},
+		];
+	},
+};
 
 module.exports = nextConfig;
